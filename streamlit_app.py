a7ce8c9f6ae6544844aca3210a06f3bf4502fbee
Nihal Thomas <105119174+NihalThomas@users.noreply.github.com>
2024-06-23 13:14:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import re
 import numpy as np
 import pandas as pd
+import validators
 
 # Load the tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
@@ -13,12 +14,19 @@ model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-mu
 
 # Function to fetch reviews from Yelp
 def fetch_reviews(yelp_url):
-    r = requests.get(yelp_url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    regex = re.compile('.*comment.*')
-    results = soup.find_all('p', {'class': regex})
-    reviews = [result.text for result in results]
-    return reviews
+    if not validators.url(yelp_url):
+        return None, "Invalid URL"
+    try:
+        r = requests.get(yelp_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        regex = re.compile('.*comment.*')
+        results = soup.find_all('p', {'class': regex})
+        reviews = [result.text for result in results]
+        if not reviews:
+            return None, "No reviews found"
+        return reviews, None
+    except requests.exceptions.RequestException as e:
+        return None, f"An error occurred: {e}"
 
 # Function to perform sentiment analysis
 def sentiment_score(review):
@@ -34,12 +42,12 @@ yelp_url = st.text_input('Enter Yelp URL')
 
 if yelp_url:
     with st.spinner('Fetching and analyzing reviews...'):
-        reviews = fetch_reviews(yelp_url)
+        reviews, error = fetch_reviews(yelp_url)
         
-        if reviews:
+        if error:
+            st.error(error)
+        else:
             df = pd.DataFrame(np.array(reviews), columns=['review'])
             df['sentiment'] = df['review'].apply(lambda x: sentiment_score(x[:512]))
 
             st.write(df)
-        else:
-            st.write("No reviews found or invalid URL.")
